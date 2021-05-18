12229b495b3a2c5e98d25981d427a9af375f3e43
Rashika_98_Dev <rashikamr1998@gmail.com>
2021-05-18 18:53:44 +0530

    Created HeroSection.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,42 @@
-import React , {useState} from 'react'
+import React , {useState , useEffect} from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from './Button';
+import './Navbar.css';
+
 
 function Navbar() {
 
     const [click , setClick] = useState(false);
 
+    const [button, setButton] = useState(true);
+
     const handleClick = () => setClick(!click);
 
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false);
+        } 
+        else{
+            setButton(true);
+        }
+    };
+
+
+    useEffect(() => {
+        showButton()
+    }, []);
+
+    window.addEventListener('resize', showButton);
 
 
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
-                        RMRM <i className="fab fa-typo3"></i>
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                        Hello <i className="fab fa-typo3"></i>
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
@@ -44,6 +65,11 @@ function Navbar() {
                         </li>
 
                     </ul>
+
+
+                    {button && <Button buttonStyle='btn--outline' buttonSize='btn--medium'>SIGN UP</Button>}
+
+
                 </div>
             </nav>
         </>
