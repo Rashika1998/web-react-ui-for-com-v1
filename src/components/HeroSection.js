2d8415c0615974016b74ea3dd797441a0e1dfd34
Rashika_98_Dev <rashikamr1998@gmail.com>
2021-05-22 14:57:37 +0530

    Hero section main part is done.

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Button } from './Button';
+import './HeroSection.css';
+import '../App.css';
 
 function HeroSection() {
     return (
         <div className="hero-container">
             <video src='../videos/video-2.mp4' autoPlay loop muted />
-            <h1>Hello everyone</h1>
-            <p>What are you waiting for..?</p>
+            <h1 id="main-topic">Hello everyone</h1>
+            <p id="main-para">Welcome to GameAppex.com by Sandushi</p>
             <div className="hero-btns">
                 <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">
                     Get Started
+                </Button>
+                <Button className="btns" buttonStyle="btn--primary" buttonSize="btn--large">
+                    Watch Trailer <i className="far fa-play-circle" />
                 </Button>
             </div>
         </div>
