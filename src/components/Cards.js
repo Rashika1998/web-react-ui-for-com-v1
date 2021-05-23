c24f623b500f9960b4e58aa769fa00c517f4f506
Rashika_98_Dev <rashikamr1998@gmail.com>
2021-05-23 07:50:39 +0530

    Adds more cards. Success...!

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,13 +6,39 @@ function Cards() {
     return (
         <div className='cards'>
             <h1>Check Out this...!</h1>
-            <div className="cards__container">
-                <div className="cards__wrapper">
-                    <ul className="cards__items">
+            <div className='cards__container'>
+                <div className='cards__wrapper'>
+                    <ul className='cards__items'>
                         <CardItem
-                        src="../images/img-9.jpg"
-                        text="Study everything you want to learn"
+                        src='../images/img-9.jpg'
+                        text='Study everything you want to learn'
                         label='Knowledge'
+                        path='/services'
+                        />
+                        <CardItem
+                        src='../images/img-2.jpg'
+                        text='Write some little programs.'
+                        label='Practical'
+                        path='/services'
+                        />
+                    </ul>
+                    <ul className='cards__items'>
+                    <CardItem
+                        src='../images/img-9.jpg'
+                        text='Study everything you want to learn'
+                        label='Knowledge'
+                        path='/services'
+                        />
+                        <CardItem
+                        src='../images/img-2.jpg'
+                        text='Write some little programs.'
+                        label='Practical'
+                        path='/services'
+                        />
+                        <CardItem
+                        src='../images/img-2.jpg'
+                        text='Write some little programs.'
+                        label='Practical'
                         path='/services'
                         />
                     </ul>
